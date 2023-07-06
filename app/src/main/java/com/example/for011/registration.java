a1ed24db024b0cfa0af57bc4c884447c3b2246b6
RoberthTR <roberthtr81@gmail.com>
2023-07-06 17:54:25 -0500

    tercera01

---
+++ app/src/main/java/com/example/for011/registration.java
@@ -0,0 +1,24 @@
+package com.example.for011;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+
+
+
+
+
+
+
+
+
+}
